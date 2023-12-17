ce14f1cccab14b7acb1f0c3cdd9601a0765a671e
Neville D'Souza <250913@usn.no>
2023-12-17 15:52:48 +0100

    Content bar
    Content bar added on the left

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ document.getElementById('nextBtn').addEventListener('click', function() {
     changeSlide(1);
 });
 
+document.querySelectorAll('#contentBar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 function changeSlide(move) {
     const newSlide = currentSlide + move;
     if (newSlide > 0 && newSlide <= totalSlides) {
